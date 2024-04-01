6274ffdc377d6426e97cd7cb5c4bc62686213899
torobo <arita.tomoki@keio.jp>
2024-04-01 11:18:30 +0900

    add vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import app from './app';
 
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
